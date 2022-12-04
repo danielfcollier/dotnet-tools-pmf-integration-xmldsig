5c0ee70f4916b95179454d2df06e23c5f9a3ab57
danielfcollier <danielfcollier@gmail.com>
2022-12-04 19:36:49 -0300

    Add Swagger autogenerated documentation.

--- API.Rest/Program.cs
+++ API.Rest/Program.cs
@@ -2,11 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseAuthorization();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 if (app.Environment.EnvironmentName != "Test")
 {
