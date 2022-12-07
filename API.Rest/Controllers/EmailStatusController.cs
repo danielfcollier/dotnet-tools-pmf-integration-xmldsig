cff11a1ab9d64896488476cf5a3e6289dcf7004a
danielfcollier <danielfcollier@gmail.com>
2022-12-07 09:43:05 -0300

    Add basic structure for new routers controllers.

--- API.Rest/Controllers/EmailStatusController.cs
+++ API.Rest/Controllers/EmailStatusController.cs
@@ -8,10 +8,10 @@ namespace App.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class InvoiceController : ControllerBase
+public class EmailStatusController : ControllerBase
 {
-    [HttpGet("{id}")]
-    public async Task<ActionResult> GetInvoice(string id)
+   [HttpGet("{id}")]
+    public async Task<ActionResult> GetEmailStatus(string id)
     {
         try
         {
@@ -36,10 +36,10 @@ public class InvoiceController : ControllerBase
 
                 // 5) Process Payload
                 foreach (var item in payloads)
-                {
-                    (XmlDocument payload, string payloadId) = item;
+                 {
+                    (XmlDocument payload, _) = item;
 
-                    var (response, statusCode) = await consumer.Run(payloadId, payload);
+                    var (response, statusCode) = await consumer.Validate(id, payload);
 
                     // 6) Save Response
                     // if (statusCode is not HttpStatusCode.OK)
@@ -62,7 +62,7 @@ public class InvoiceController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult> GetInvoice([FromQuery(Name = "year")] string? year, [FromQuery(Name = "month")] string? month)
+    public async Task<ActionResult> GetEmailStatus([FromQuery(Name = "year")] string? year, [FromQuery(Name = "month")] string? month)
     {
         try
         {
@@ -112,29 +112,8 @@ public class InvoiceController : ControllerBase
         }
     }
 
-    /// <summary>
-    /// Creates a TodoItem.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns>A newly created TodoItem</returns>
-    /// <remarks>
-    /// Sample request:
-    ///
-    ///     POST /Todo
-    ///     {
-    ///        "id": 1,
-    ///        "name": "Item #1",
-    ///        "isComplete": true
-    ///     }
-    ///
-    /// </remarks>
-    /// <response code="201">Returns the newly created item</response>
-    /// <response code="400">If the item is null</response>
-    [HttpPost("{id}")]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [Produces("application/json")]
-    public async Task<ActionResult> PostInvoice(string id, [FromBody] Customer customer)
+   [HttpPut("{id}")]
+    public async Task<ActionResult> PutEmailStatus(string id, [FromBody] Customer customer)
     {
         try
         {
@@ -159,63 +138,7 @@ public class InvoiceController : ControllerBase
 
                 // 5) Process Payload
                 foreach (var item in payloads)
-                {
-                    (XmlDocument payload, string payloadId) = item;
-
-                    var (response, statusCode) = await consumer.Validate(payloadId, payload);
-
-                    // 6) Save Response
-                    // if (statusCode is not HttpStatusCode.OK)
-                    // {
-
-                    // }
-
-                    // XmlDocument xmlResponse = new() { PreserveWhitespace = true };
-                    // xmlResponse.Load(response);
-                    // InvoiceResponse result = XmlHandler.DeserializeResponse<InvoiceResponse>(xmlResponse);
-                }
-            }
-
-            return Ok();
-        }
-        catch (Exception error)
-        {
-            return BadRequest(error.Message);
-        }
-    }
-
-    /// <summary>
-    /// Deletes a specific TodoItem.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteInvoice(string id)
-    {
-        try
-        {
-            {
-                string partnerId = "#0001";
-
-                // TODO: 1) Authentication (JWT)
-
-                // 2) Get Partner Data
-                Partner? partner = await DBHandler.GetPartnerData(partnerId);
-                if (partner is null)
-                {
-                    return NotFound();
-                }
-
-                // 3) Start Jobs
-                Producer producer = new(partner);
-                Consumer consumer = new(partner);
-
-                // 4) Produce Payload
-                List<(XmlDocument, string)> payloads = await producer.GetPayloadsToProcess();
-
-                // 5) Process Payload
-                foreach (var item in payloads)
-                {
+                 {
                     (XmlDocument payload, _) = item;
 
                     var (response, statusCode) = await consumer.Validate(id, payload);
