33d96b1f0e0654b4871e331d5835a2bbebf1b720
Daniel Collier <danielfcollier@gmail.com>
2023-08-17 20:24:37 -0300

    chore: remove unused code

--- PMF.Invoice.Service/Handlers/UtilHandlers.cs
+++ PMF.Invoice.Service/Handlers/UtilHandlers.cs
@@ -4,21 +4,11 @@ namespace Handlers;
 
 public static class UtilsHandler
 {
-    public static string MaskPersonDocument(string documentId)
+    public static string MaskDocument(string documentId)
     {
         string cleanDocument = OnlyNumbers(documentId);
 
         return cleanDocument.PadLeft(11, '0');
-    }
-
-    public static string MaskDate(string date)
-    {
-        var temp = date.Split('/');
-        var year = temp[2];
-        var month = temp[1];
-        var day = temp[0];
-
-        return $"{year}-{month}-{day}";
     }
 
     public static string OnlyNumbers(string input)
